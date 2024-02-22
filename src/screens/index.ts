8f27257bf02f7a7da2644c0f8c4bdadf0597dde1
ciekotics <rahul.saxena@jabotics.in>
2024-02-22 19:30:34 +0530

    Login and Signupinitial setup

---
+++ src/screens/index.ts
@@ -0,0 +1,9 @@
+import Welcome from './welcome';
+import SignUp from './signup';
+import Login from './login';
+
+export {
+  Welcome,
+  SignUp,
+  Login
+}
---
+++ src/screens/welcome/welcome.tsx
@@ -0,0 +1,104 @@
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import COLORS from "../../utils/constants/colors";
+import Button from "../../components/Button";
+
+export default function Welcome({ navigation }: { navigation: any }) {
+  return (
+    <LinearGradient
+      style={{
+        flex: 1,
+      }}
+      colors={[COLORS.secondary, COLORS.primary]}
+    >
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <View
+          style={{
+            paddingHorizontal: 22,
+            position: "absolute",
+            top: 400,
+            width: "100%",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 50,
+              fontWeight: "800",
+              color: COLORS.white,
+            }}
+          >
+            Let&apos;s Get
+          </Text>
+          <Text
+            style={{
+              fontSize: 46,
+              fontWeight: "800",
+              color: COLORS.white,
+            }}
+          >
+            Started
+          </Text>
+
+          <View style={{ marginVertical: 22 }}>
+            <Text
+              style={{
+                fontSize: 16,
+                color: COLORS.white,
+                marginVertical: 4,
+              }}
+            >
+              Welcome to the Sports Arena App!
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: COLORS.white,
+              }}
+            >
+              Discover upcoming matches and events.
+            </Text>
+          </View>
+
+          <Button
+            title="Join Now"
+            onPress={() => navigation.navigate("SignUp")}
+            style={{
+              marginTop: 22,
+              width: "100%",
+            }}
+          />
+
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 12,
+              justifyContent: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                color: COLORS.white,
+              }}
+            >
+              Already have an account ?
+            </Text>
+            <Pressable onPress={() => navigation.navigate("Login")}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: COLORS.white,
+                  fontWeight: "bold",
+                  marginLeft: 4,
+                }}
+              >
+                Login
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </LinearGradient>
+  );
+}
