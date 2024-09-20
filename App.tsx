a72357bc659ae84726a7370fd8f37becbf041beb
ciekotics <rahul.saxena@jabotics.in>
2024-09-20 17:52:53 +0530

    changes

--- App.tsx
+++ App.tsx
@@ -2,11 +2,20 @@ import React from "react";
 // import { SafeAreaView, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 // CUSTOM IMPORTS
 import Router from "./src/components/router";
 
 import { store } from "./src/store";
+
+export type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+  GroundList: undefined;
+}
+
+export type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
 
 export default function App() {
   return (
